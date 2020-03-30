d4005920943ddb590e19106d2f29a0977c4ff458
thisoban mahalingam <thisoban_1995@hotmail.com>
2020-03-30 16:16:56 +0200

    Add project files.

---
+++ maatwerk-kassasysteem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace maatwerk_kassasysteem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
